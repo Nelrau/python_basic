7b201a8e1297b3210be406aeb603c92c30ee0bba
Nelrau <75430602+Nelrau@users.noreply.github.com>
2023-04-02 08:03:24 -0300

    Update jogar.py
    importa forca.py com opções de quer jogar ou não

--- jogar.py
+++ jogar.py
@@ -1,2 +1,8 @@
-from Forca import jogar
-jogar()
+import forca
+print('s vc joga forca\nn vc sai')
+forca.msg('Menu')
+d = input('Quer jogar? ')
+if 's' == d:
+	forca.msg('Bem vindo ao jogo da forca')
+	forca.jogar()
+forca.msg('Fim de jogo')
